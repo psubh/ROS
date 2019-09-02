c1499929a0f38e4fe53107bb83d637f60a524d95
psubh <psubh9406@gmail.com>
2019-09-03 03:01:55 +0530

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/subhash/catkin_ws/src
+# Build directory: /home/subhash/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ros_basic_tutorials")
+subdirs("ros_essentials_cpp")
---
+++ devel/share/gennodejs/ros/ros_essentials_cpp/msg/_index.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+let IoTSensor = require('./IoTSensor.js');
+let FibonacciActionGoal = require('./FibonacciActionGoal.js');
+let FibonacciGoal = require('./FibonacciGoal.js');
+let FibonacciAction = require('./FibonacciAction.js');
+let FibonacciFeedback = require('./FibonacciFeedback.js');
+let FibonacciResult = require('./FibonacciResult.js');
+let FibonacciActionResult = require('./FibonacciActionResult.js');
+let FibonacciActionFeedback = require('./FibonacciActionFeedback.js');
+
+module.exports = {
+  IoTSensor: IoTSensor,
+  FibonacciActionGoal: FibonacciActionGoal,
+  FibonacciGoal: FibonacciGoal,
+  FibonacciAction: FibonacciAction,
+  FibonacciFeedback: FibonacciFeedback,
+  FibonacciResult: FibonacciResult,
+  FibonacciActionResult: FibonacciActionResult,
+  FibonacciActionFeedback: FibonacciActionFeedback,
+};
